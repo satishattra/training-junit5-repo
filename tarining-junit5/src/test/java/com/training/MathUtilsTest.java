8189cd10215cfe316537c6e5ee299a7236c36ce6
satish.kumar1 <satish.kumar1@BCP-ATLT1436.attragroup.com>
2021-07-12 14:22:18 +0530

    first commit

---
+++ tarining-junit5/src/test/java/com/training/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.training;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("This is a test run");
+	}
+
+}
