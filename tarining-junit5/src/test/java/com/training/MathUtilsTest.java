7a4986d6f1527fd9f13e41b338b31731784320cc
satish.kumar1 <satish.kumar1@BCP-ATLT1436.attragroup.com>
2021-07-12 18:41:58 +0530

    Added some new test methods

--- tarining-junit5/src/test/java/com/training/MathUtilsTest.java
+++ tarining-junit5/src/test/java/com/training/MathUtilsTest.java
@@ -13,5 +13,11 @@ class MathUtilsTest {
 		int actual = mathUtils.add(1, 0);
 		assertEquals(expected, actual, "The add method should add two numbers");
 	}
+	
+	@Test
+	void testComputeCircle() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+	}
 
 }
